712657a3df7fd57fcacc2cc2164844a9605ffb2f
Dmitry Maslennikov <maslennikovdm@gmail.com>
2020-03-29 21:35:53 +0300

    First release to GH pages

---
+++ docs/precache-manifest.2d884b84a4c00d38180169202e939bfc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fd4013eaf5f28890f7de02c2e95edae",
+    "url": "/invest/index.html"
+  },
+  {
+    "revision": "a636af3ac68ff5685841",
+    "url": "/invest/static/css/2.90ca997e.chunk.css"
+  },
+  {
+    "revision": "32cfb701d38ff8f3204e",
+    "url": "/invest/static/css/main.3e32ed87.chunk.css"
+  },
+  {
+    "revision": "a636af3ac68ff5685841",
+    "url": "/invest/static/js/2.6551d466.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/invest/static/js/2.6551d466.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32cfb701d38ff8f3204e",
+    "url": "/invest/static/js/main.a27a4c58.chunk.js"
+  },
+  {
+    "revision": "3adb5581ae510f1f2ed6",
+    "url": "/invest/static/js/runtime-main.04758877.js"
+  }
+]);
