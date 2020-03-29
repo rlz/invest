01a5cd18879dd8dc648f677857864e6a70544320
Dmitry Maslennikov <maslennikovdm@gmail.com>
2020-03-29 20:27:48 +0300

    Better currencies formatter (spaces-comma)

--- src/widgets/spans.tsx
+++ src/widgets/spans.tsx
@@ -3,19 +3,23 @@ import { Currency } from '../api/common';
 import { c } from '../tools/react';
 import "./spans.scss";
 
+export function fNum (v: number): string {
+  return (Math.round(v * 100) / 100).toLocaleString("ru-RU");
+}
+
 export const Rub = (props: { v: number; color?: boolean }): JSX.Element => {
   const cls = c('currency rub', [props.v > 0, "positive"], [props.v < 0, "negative"], [props.color === true, "color"]);
-  return <span className={cls}>{'\u20BD'}{props.v.toFixed(2)}</span>;
+  return <span className={cls}>{'\u20BD'} {fNum(props.v)}</span>;
 };
 
 export const Usd = (props: { v: number; color?: boolean }): JSX.Element => {
   const cls = c('currency usd', [props.v > 0, "positive"], [props.v < 0, "negative"], [props.color === true, "color"]);
-  return <span className={cls}>${props.v.toFixed(2)}</span>;
+  return <span className={cls}>$ {fNum(props.v)}</span>;
 };
 
 export const Eur = (props: { v: number; color?: boolean }): JSX.Element => {
   const cls = c('currency eur', [props.v > 0, "positive"], [props.v < 0, "negative"], [props.color === true, "color"]);
-  return <span className={cls}>{'\u20AC'}{props.v.toFixed(2)}</span>;
+  return <span className={cls}>{'\u20AC'} {fNum(props.v)}</span>;
 };
 
 export const Cur = (props: { t: Currency; v: number; color?: boolean }): JSX.Element => {
@@ -32,5 +36,5 @@ export const Cur = (props: { t: Currency; v: number; color?: boolean }): JSX.Ele
 
 export const Prc = (props: { v: number; color?: boolean }): JSX.Element => {
   const cls = c('percent', [props.v > 0, "positive"], [props.v < 0, "negative"], [props.color === true, "color"]);
-  return <span className={cls}>{(props.v * 100).toFixed(2)}%</span>;
+  return <span className={cls}>{fNum(props.v * 100)}%</span>;
 };
