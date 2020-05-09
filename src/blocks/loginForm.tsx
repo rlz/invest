909db3e58c139d06b22fbf899dc97091e9742ef5
Dmitry Maslennikov <maslennikovdm@gmail.com>
2020-05-10 02:42:10 +0300

    Add disclaimer

--- src/blocks/loginForm.tsx
+++ src/blocks/loginForm.tsx
@@ -22,15 +22,51 @@ export class LoginForm extends React.Component<Props, State> {
   render (): JSX.Element {
     return (
       <div className='bLoginForm'>
+        <div className='description'>
+          <p>
+            Приложение анализирует насколько успешно вы торгуете через сервис «Тинькофф Инвестиции».
+          </p>
+          <p>
+            Приложению требуется API токен только для чтения ваших транзакций
+            — приложение не способно совершать операции на бирже.
+            Токен используется только для чтения и остается внутри вашего браузера, как и все ваши данные.
+          </p>
+          <p>
+            Исходный код приложения доступен для аудита на{' '}
+            <a target='blank' href='https://github.com/rlz/invest'>GitHub</a>.
+          </p>
+          <p>
+            Получениие API токена описано в{' '}
+            <a target='blank' href='https://tinkoffcreditsystems.github.io/invest-openapi/auth/'>
+              документации к «Тинькофф Инвестиции OpenApi»
+            </a>.
+          </p>
+        </div>
+        <div className='disclaimer'>
+          <h1>Внимание</h1>
+          <p>
+            Данное приложение не является официальным прииложениием Тинькофф,
+            но созданно независимым разработчиком в исследовательских целях.
+          </p>
+          <p>
+            Вводя свой API токен вы подтверждаете, что осознаете все риски,
+            связанные с использованием приложения и берете их на себя.
+          </p>
+          <p>
+            Приложение предоставляется "как есть" без каких-либо гарантий
+            и возможно содержит недоработки и ошибки реализации, за которые автор не несет
+            никакой ответственности.
+          </p>
+        </div>
         <form>
           <input
             type='text'
             value={this.state.token}
             onChange={(ev): void => this.setState({ token: ev.target.value })}
-            placeholder='API Token'
+            placeholder='API Токен'
             autoFocus
           />
-          <button type='button' onClick={(): void => this.onLoginClick()}>Login</button>
+          <button type='button' onClick={(): void => this.onLoginClick()}>Войти</button>
         </form>
       </div>
     );
